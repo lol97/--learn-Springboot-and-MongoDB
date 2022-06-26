25723ccb1ae982e36478e0fb7549b15c820c0bd3
lol97 <blackmanga80@gmail.com>
2022-06-26 22:18:06 +0700

    first init project

---
+++ src/main/java/com/aglistech/mongoboot/ECommerceProject1Application.java
@@ -0,0 +1,13 @@
+package com.aglistech.mongoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProject1Application.class, args);
+	}
+
+}
