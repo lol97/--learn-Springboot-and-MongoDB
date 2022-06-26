02542a4c9d3b009b06dfd54b83890e126e75aae7
lol97 <blackmanga80@gmail.com>
2022-06-26 22:30:17 +0700

    change the mainclass name to SpringMongoApplication

--- src/main/java/com/aglistech/mongoboot/SpringMongoApplication.java
+++ src/main/java/com/aglistech/mongoboot/SpringMongoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceProject1Application {
+public class SpringMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceProject1Application.class, args);
+		SpringApplication.run(SpringMongoApplication.class, args);
 	}
 
 }
--- src/test/java/com/aglistech/mongoboot/SpringMongoApplicationTests.java
+++ src/test/java/com/aglistech/mongoboot/SpringMongoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ECommerceProject1ApplicationTests {
+class SpringMongoApplicationTests {
 
 	@Test
 	void contextLoads() {
